dd54d28495c28dbf015eada15fc19fec82c2c1ec
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-02-02 22:57:25 -0200

    First ideas and 2 pending specs

--- lib/job.rb
+++ lib/job.rb
@@ -2,16 +2,27 @@ class Job
   class SelfDependencyError < StandardError; end  
   class CircularDependencyError < StandardError; end
 
+  attr_accessor :ordered
+  attr_accessor :rules
+
   def initialize(jobs_input)
+    parse(jobs_input)
+    @ordered = order(first_order)
   end
 
   def parse(input)
+    @rules = input.scan(/(\w*) => ?(\w*)/)
+    raise SelfDependencyError, "Jobs cannot depend on themselves" unless @rules.select{|job, dependency| job == dependency}.empty?
   end
 
   def first_order
+    return @rules.map{|node| node[0]}
   end
 
   def order(job_array)
+    job_array.each do |job_node|      
+    end
+    job_array
   end
 
   def get_dependent(job_node, job_array)
---
+++ spec/job_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require_relative '../lib/job'
+
+describe Job do
+  context "when input is an empty list" do
+    it "should return a blank array" do
+      pending
+    end
+  end
+
+  context "when input is a single job" do
+    it "should return an array with the job" do
+      pending
+    end
+  end
+end
