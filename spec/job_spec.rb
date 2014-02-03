47c618dbafee482d0abf8a1e7a967b9aa7e26e3e
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-02-02 23:41:47 -0200

    Tested with longer job list - missing only exceptions

--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -23,14 +23,28 @@ describe Job do
     end
   end
 
-  context "when giving a simple dependency job list" do
+  context "when giving a dependency job list" do
     it "should return a sequence with dependent job after its dependency" do
       job = Job.new("a =>\nb =>c\nc =>")
       expect(job.ordered.to_s).to match /c.*b/
+
+      job2 = Job.new("a =>\nb => c\nc => f\nd => a\ne => b\nf =>")
+      expect(job2.ordered.to_s).to match /f.*c/
+      expect(job2.ordered.to_s).to match /c.*b/
+      expect(job2.ordered.to_s).to match /b.*e/
+      expect(job2.ordered.to_s).to match /a.*d/
+    end
+  end
+
+  context "when giving a circular dependency job list" do
+    it "should raise an exception" do
+      pending
     end
   end
 
   context "when giving a self dependency job list" do
-
+    it "should raise an exception" do
+      pending
+    end
   end
 end
