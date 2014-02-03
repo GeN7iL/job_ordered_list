574a55785e1dabdbe95b2372b7235d3acade0b18
Felipe Pelizaro Gentil <cdigentil@gmail.com>
2014-02-02 23:01:12 -0200

    rules and first ordered job order working

--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -4,13 +4,22 @@ require_relative '../lib/job'
 describe Job do
   context "when input is an empty list" do
     it "should return a blank array" do
-      pending
+      job = Job.new("")
+      expect(job.ordered).to eq([])
     end
   end
 
   context "when input is a single job" do
     it "should return an array with the job" do
-      pending
+      job = Job.new("a =>")
+      expect(job.ordered).to eq(["a"])
+    end
+  end
+
+  context "when giving a no dependency job list" do
+    it "should return a sequence with all jobs no matter the order" do
+      job = Job.new("a =>\nb => c\nc =>")
+      expect(job.ordered).to eq(["a", "b", "c"])
     end
   end
 end
